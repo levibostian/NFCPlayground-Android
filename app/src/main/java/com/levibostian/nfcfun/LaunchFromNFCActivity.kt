63a53a8d427d40e407f2b9f4cadc6006e85df2a0
Levi Bostian <levi.bostian@gmail.com>
2018-04-11 12:36:33 -0500

    Setup app to scan NFC tag of URL and launch app from it.

---
+++ app/src/main/java/com/levibostian/nfcfun/LaunchFromNFCActivity.kt
@@ -0,0 +1,41 @@
+package com.levibostian.nfcfun
+
+import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.widget.Toast
+
+class LaunchFromNFCActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "NFC_FUN"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(MainActivity.getIntent(this))
+
+        parseNfcTag()
+
+        Toast.makeText(this, "Launched app from NFC", Toast.LENGTH_LONG).show()
+    }
+
+    private fun parseNfcTag() {
+        if (intent != null && NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.let { rawMessages ->
+                val messages = arrayOfNulls<NdefMessage>(rawMessages.size)
+                for (i in 0 until rawMessages.size) {
+                    val message = rawMessages[i] as NdefMessage
+                    messages[i] = message
+                    Log.d(TAG, "Message: $message")
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/levibostian/nfcfun/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.levibostian.nfcfun
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun getIntent(context: Context): Intent {
+            return Intent(context, this::class.java)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
